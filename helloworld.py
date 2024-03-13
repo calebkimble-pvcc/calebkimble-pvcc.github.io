2493c0798572191ca0f14f5a05a234423e4344ee
calebkimble-pvcc <159074356+calebkimble-pvcc@users.noreply.github.com>
2024-03-13 12:14:22 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Caleb Kimble
+# Program: Purpouse-Hello World in Python
+
+print('Hello World! This is my first Python Program.')
+print('I can program in HTML, which is awesome.')
+print("However HTML cannot do arithmatic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
